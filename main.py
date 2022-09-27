0c7889a3a4bef3d678e678a74840d298ffcbccb2
progresso85 <martin.heuripeau@gmail.com>
2022-09-27 16:53:30 +0200

    hello world

--- main.py
+++ main.py
@@ -1,27 +1,16 @@
-from typing import Union
-
 from fastapi import FastAPI
-from pydantic import BaseModel
+
+import uvicorn
 
 app = FastAPI()
 
+#get, put, delete,
 
-class Item(BaseModel):
-    name: str
-    price: float
-    is_offer: Union[bool, None] = None
-
-
+#get
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
+async def hello_world():
+    return {"hello : world"}
 
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8000)
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
-
-
-@app.put("/items/{item_id}")
-def update_item(item_id: int, item: Item):
-    return {"item_name": item.name, "item_id": item_id}
