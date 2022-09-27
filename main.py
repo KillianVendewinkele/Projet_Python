d0324a13152a25f6f004c7ba3c4de7817ad729fb
Zook-coder <Zooklenon11@gmail.com>
2022-09-27 16:49:33 +0200

    adding file path collect

--- main.py
+++ main.py
@@ -58,3 +58,7 @@ async def get_model(model_name: ModelName):
         return {"model_name": model_name, "message": "LeCNN all the images"}
 
     return {"model_name": model_name, "message": "Have some residuals"}
+
+@app.get("/files/{file_path:path}")
+async def read_file(file_path: str):
+    return {"file_path": file_path}
