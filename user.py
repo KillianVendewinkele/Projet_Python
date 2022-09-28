1773905e66f1e826ac0902be7a53b70799741b55
KillianVendewinkele <killian.vendewinkele@gmail.com>
2022-09-28 22:55:06 +0200

    db splitted into 4 json
    Co-authored-by: Zook-coder <Zook-coder@users.noreply.github.com>
    Co-authored-by: ADJE Abed <Abed-Nego28@users.noreply.github.com>

--- user.py
+++ user.py
@@ -1,22 +1,12 @@
-import json
 from fastapi import FastAPI
 import json
 
 app = FastAPI()
-
-# Check the id_user for creating new account
-@app.get("/users")
-async def check_id_user(id_user : str):
-
-    # Recover db.json
-    with open('db.json') as file:
-        db_json = json.loads(file)
-    
-    for i in range(db_json.users):
-        if db_json.users.id_user[i] == id_user:
-            return {db_json.users[i].id_user}
-        else:
-            return id_user
+# db_file_* is used to set the same db for each function (easy to change)
+db_file_users = "users.json"
+db_file_commands = "commands.json"
+db_file_carts = "carts.json"
+db_file_products = "products.json"
 
 # Create the user
 @app.post("/users/{id_user}/{password}")
@@ -24,12 +14,25 @@ async def register(id_user : str, password : str):
         if id_user == check_id_user(id_user):
             return "User already taken"
         else:
-            return {"id_user" : id_user, "password" : password}
+            return add_user_to_db(db_file_users, id_user, password)
+
+def add_user_to_db(file_json : str, id : str , password : str):
+    f = open(str(file_json), 'r+')
+    db_json = json.load(f)
+    db_json["users"].append({
+            "id_user":id,
+            "password":password
+        })
+    f.close()
+    f = open(str(file_json), 'w')
+    json.dump(db_json, f, sort_keys=True, indent=4)
+    f.close()
+    return str(id) + "ok"
 
 # Change password
 @app.patch("users/{id_user}/{password}")
 async def change_password(id_user : str, password : str):
-    file = open('db.json', 'w')
+    file = open(db_file_users, 'w')
     db_json = json.loads(file)
     return db_json
     for i in range(db_json.users):
@@ -37,3 +40,19 @@ async def change_password(id_user : str, password : str):
             file.write()
 
     db_json.users.password = password
+
+
+
+# Check the id_user for creating new account
+def check_id_user(id_user : str):
+    
+    # Recover db.json
+    f = open(db_file_users, 'r')
+    files = f.read()
+    db_json = json.loads(files)
+    for i in range(0,len(db_json["users"])):
+        if db_json["users"][i]["id_user"] == id_user:
+            return db_json["users"][i]["id_user"]
+        else:
+            print(0)
+    return "You can create your account"
