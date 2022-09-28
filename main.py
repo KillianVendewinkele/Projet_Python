2f23afea29a54806d7721f6f2e4df17e959b5f13
progresso85 <martin.heuripeau@gmail.com>
2022-09-28 14:24:46 +0200

    ajout get composent

--- main.py
+++ main.py
@@ -4,13 +4,20 @@ import uvicorn
 
 app = FastAPI()
 
-#get, put, delete,
 
-#get
+# get, put, delete,
+
+# get
 @app.get("/")
 async def hello_world():
     return {"hello : world"}
 
+
+@app.get("/component/{}component_id")  # path parameter
+async def get_component(component_id: int):
+    # operation
+    return {"component_id": component_id}
+
+
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=8000)
-
