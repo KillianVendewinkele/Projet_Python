c1473fe4ec8abffa8e698eb2d2122cb7a7a2ab45
progresso85 <martin.heuripeau@gmail.com>
2022-09-29 20:50:08 +0200

    add get list products
    Import the list of product within a range

--- list_products.py
+++ list_products.py
@@ -9,35 +9,10 @@ db_file_carts = "carts.json"
 db_file_products = "products.json"
 
 
-# Create the product
-@app.post("/products/{id_products}")
-async def register(id_products: str):
-    if id_products == check_id_products(id_products):
-        return "User already taken"
-    else:
-        return add_products_to_db(db_file_products, id_products)
-
-
-# Add the user to the json file
-def add_products_to_db(file_json: str, id: str, password: str):
-    f = open(str(file_json), 'r+')
-    db_json = json.load(f)
-    db_json["users"].append({
-        "id_products": id
-    })
-    f.close()
-    f = open(str(file_json), 'w')
-    json.dump(db_json, f, sort_keys=True, indent=4)
-    f.close()
-    return str(id) + " ok"
-
-
-# Check the id_user for creating new account
-def check_id_products(id_user: str):
-    f = open(db_file_users, 'r')
+@app.get("/products")
+async def read_products(category: str, description: str, id_product: str, price: str, product: str, stock: str):
+    f = open(db_file_products, 'r')
     files = f.read()
     db_json = json.loads(files)
-    for i in range(0, len(db_json["products"])):
-        if db_json["products"][i]["id_products"] == id_user:
-            return db_json["users"][i]["id_products"]
-    return "You can create your products"
+    return {"category": category, "description": description, "id_product": id_product, "price": price,
+            "product": product, "stock": stock}
