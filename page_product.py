354dd2d437ebd1a22b729f145d9df6a7f008d986
KillianVendewinkele <killian.vendewinkele@gmail.com>
2022-09-29 12:48:51 +0200

    product_details() added

---
+++ page_product.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI
+import json
+
+app = FastAPI()
+# db_file_* is used to set the same db for each function (easy to change)
+db_file_users = "users.json"
+db_file_commands = "commands.json"
+db_file_carts = "carts.json"
+db_file_products = "products.json"
+
+# Get the product name, price and description
+@app.get("/product/{id_product}")
+async def product_details(id_product : str):
+
+     # Recover db.json
+    f = open(db_file_products, 'r')
+    db_json = json.load(f)
+    for i in range(0,len(db_json["products"])):
+        if db_json["products"][i]["id_product"] == id_product:
+            return db_json["products"][i]["id_product"], db_json["products"][i]["price"], db_json["products"][i][""]
+    return "The selected product doesn't exist"
